8b972033c36c06b43fd1bce9bb7eb3ad60693b5f
ABHISHEK MISHRA <51957554+Abhishekmishra-17@users.noreply.github.com>
2019-10-20 15:19:32 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,10 @@
+ch=str(input('enter the character:'))
+list=['a','e','i','o','u','A','E','I','O','U']
+for i in range(0,10):
+        if (ch==list[i]):
+           print('the given character is vowel')
+           break
+if(ch!=list[i]):
+        print('not vowel')
+  
+
