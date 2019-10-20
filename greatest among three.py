bc3793924ca17b1f9ce69deee7e679b51c461c92
ABHISHEK MISHRA <51957554+Abhishekmishra-17@users.noreply.github.com>
2019-10-20 15:02:04 +0530

    Add files via upload

---
+++ greatest among three.py
@@ -0,0 +1,15 @@
+a=float(input('enter the first number:'))
+b=float(input('enter the second number:'))
+c=float(input('enter the third number:'))
+if(a>b):
+    if(a>c):
+        print('a is greater')
+    else:
+        print('c is greatet')
+elif(b>c):
+    print('b is greatest')
+elif(a==b==c):
+        print('all are equal')
+else:
+            print('c is greatest ')
+
