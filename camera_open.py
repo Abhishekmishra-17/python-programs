8b57db5bb048f92c6cd4a8b72f7b28000f78a0ea
ABHISHEK MISHRA <51957554+Abhishekmishra-17@users.noreply.github.com>
2019-10-20 16:11:47 +0530

    Update camera_open.py

--- camera_open.py
+++ camera_open.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2
-cap=cv2.VideoCapture(1)
+cap=cv2.VideoCapture(1) # replace 1 to 0,1,2,-1 as your camera permission
 while(True):
     ret,img=cap.read()
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     cv2.imshow('img',gray)
-    if cv2.waitKey(27) & 0xFF==ord('q'):
+    if cv2.waitKey(27) & 0xFF==ord('q'):# press "q" to quit the frame
         break
 cap.release()
 cv2.destroyAllWindows()
